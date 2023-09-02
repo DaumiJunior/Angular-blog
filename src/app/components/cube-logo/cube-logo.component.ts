4fe37ab637628d8b05a90f96309f8093c95c4dfd
daumi <140564095+DaumiJunior@users.noreply.github.com>
2023-09-02 00:10:21 -0300

    two commit

---
+++ src/app/components/cube-logo/cube-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cube-logo',
+  templateUrl: './cube-logo.component.html',
+  styleUrls: ['./cube-logo.component.css']
+})
+export class CubeLogoComponent {
+
+}
